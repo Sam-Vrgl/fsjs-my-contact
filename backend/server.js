1cfa5fd29a3f203c70a8e9430d6cfb0cbc6a8a9c
Samuel Vergnol <46886716+Sam-Vrgl@users.noreply.github.com>
2025-09-17 11:00:05 +0200

    Backend DB connection and starter server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
